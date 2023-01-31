875304a40fb179a0dd21c7091189e7172f3f9629
Iceows <mounierr07@gmail.com>
2023-01-31 22:17:09 +0100

    Fix EMUI 9

--- gnss_supl20service_hisi/make-pra.cmd
+++ gnss_supl20service_hisi/make-pra.cmd
@@ -7,7 +7,7 @@ rd /s /q .\supl20\
 
 REM Genere les fichier smali 
 REM Need to change location of framework android (vndk26 version huawei)
-java -jar baksmali-2.5.2.jar deodex -a 26 -o .\supl20\PRA\src-out -b  ..\..\huawei-26\framework\arm64\boot.oat ".\original\PRA\gnss_supl20service_hisi.apk_classes.dex"
+java -jar baksmali-2.5.2.jar deodex -a 26 -o .\supl20\PRA\src-out -b  ..\..\vendor_hi6250\sdk26\framework\arm64\boot.oat ".\original\PRA\gnss_supl20service_hisi.apk_classes.dex"
 
 REM Decompile APK, extrait le manifest dans le repertoire cible
 java -jar apktool_2.6.0.jar decode -f -o .\supl20\PRA\apk-out .\original\PRA\gnss_supl20service_hisi.apk
@@ -39,9 +39,18 @@ FOR /R ".\supl20\PRA\src-out" %%f IN (*.smali) DO  (
 
 REM -------------------------------------------------
 
+REM Recopie le nouveau fichier ConfigManager avec le chemin vers system
+REM "/system/etc/gnss/config/gnss_suplconfig_hisi.xml"
+xcopy /Y .\modded\ConfigManager.smali  .\supl20\PRA\src-out\com\android\supl\config\
+
+
 
 REM Fabrique le ficher classes.dex  a partir des fichier smali, android 26
-java -jar smali-2.5.2.jar a --api 26 .\supl20\PRA\src-out -o .\supl20\PRA\apk-out\classes.dex
+java -jar smali-2.5.2.jar a --api 26  .\supl20\PRA\src-out -o .\supl20\PRA\apk-out\classes.dex
+
+REM Recopie le nouveau fichier AndroidManifest.xml
+xcopy /Y .\modded\AndroidManifest.xml  .\supl20\PRA\apk-out\AndroidManifest.xml
+
 
 REM Recompile APK  dans le repertoire cible
 java -jar apktool_2.6.0.jar build -o .\supl20\PRA\recompiled.apk  .\supl20\PRA\apk-out
@@ -58,7 +67,7 @@ pause
 
 REM envoie le fichier sur le telephone
 adb root
-adb remount rw, /system
+adb remount /system
 adb push .\apk\gnss_supl20service_hisi.apk /system/app/gnss_supl20service_hisi/gnss_supl20service_hisi.apk
 
 pause
